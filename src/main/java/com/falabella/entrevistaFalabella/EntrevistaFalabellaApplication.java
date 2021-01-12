9a5c1c31f13009e9b901e0f6ffe3199e8bb66231
Felipe_Choque <felipe.choque@rycconsultores.cl>
2021-01-12 10:49:09 -0300

    base proyecto java

---
+++ src/main/java/com/falabella/entrevistaFalabella/EntrevistaFalabellaApplication.java
@@ -0,0 +1,13 @@
+package com.falabella.entrevistaFalabella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistaFalabellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistaFalabellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falabella/entrevistaFalabella/EntrevistaFalabellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falabella.entrevistaFalabella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaFalabellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
