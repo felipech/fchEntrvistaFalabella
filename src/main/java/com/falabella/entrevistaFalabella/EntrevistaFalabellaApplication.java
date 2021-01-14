48b04cfb7eab5e5287b214398c50fc5f640f5192
Felipe_Choque <felipe.choque@rycconsultores.cl>
2021-01-14 18:45:06 -0300

    se cambia version swagger

--- src/main/java/com/falabella/entrevistaFalabella/EntrevistaFalabellaApplication.java
+++ src/main/java/com/falabella/entrevistaFalabella/EntrevistaFalabellaApplication.java
@@ -2,7 +2,15 @@ package com.falabella.entrevistaFalabella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.google.common.base.Predicate;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -12,5 +20,17 @@ public class EntrevistaFalabellaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EntrevistaFalabellaApplication.class, args);
 	}
+	
+	 @Bean
+	 public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.any())
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	    }
+	
+
+	
 
 }
--- src/main/java/com/falabella/entrevistaFalabella/model/ReglasProductos.java
+++
@@ -1,7 +0,0 @@
-package com.falabella.entrevistaFalabella.model;
-
-public interface ReglasProductos {
-	
-	public Productos calculoReglas(Productos producto, int dia);
-
-}
